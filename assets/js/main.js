464b0d654596895964957efba582736a95461305
Ricardo Miranda <ricardo.a.miranda@outlook.com>
2022-07-25 15:32:46 -0300

    v1.2 - Adicionado calculo de impostos.

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,6 +14,11 @@ function calcVlrImp(num1, num2) {
     return num1 - num2;
 }
 
+function tributos(num1, num2) {
+
+    return num1 * num2
+}
+
 window.onload = function () {
     var objValorBruto = document.getElementById("valor_bruto");
     var objValorLiquido = document.getElementById("valor_liq");
@@ -21,7 +26,16 @@ window.onload = function () {
     var objResValorBruto = document.getElementById("res_valor_bruto");
     var objResValorLiq = document.getElementById("res_valor_liq");
     var objResValorImp = document.getElementById("res_valor_imposto");
+    var objResIRRF = document.getElementById("res_irrf");
+    var objResCSLL = document.getElementById("res_csll");
+    var objResPIS = document.getElementById("res_pis");
+    var objResCOFINS = document.getElementById("res_cofins");
     var objbotao = document.getElementById("btn_calcular");
+
+    var irrf = 0.015
+    var csll = 0.01
+    var pis = 0.0065
+    var cofins = 0.03
 
     objbotao.onclick = function () {
         var objFloBruto = parseFloat(objValorBruto.value);
@@ -35,6 +49,10 @@ window.onload = function () {
             objResValorBruto.innerHTML = "Valor Bruto: R$" + bruto;
             objResValorLiq.innerHTML = "Valor Líquido: R$" + objFloLiq.toFixed(2);
             objResValorImp.innerHTML = "Valor de Imposto: R$" + calcVlrImp(bruto, objFloLiq).toFixed(2);
+            objResIRRF.innerHTML = "IRRF: R$" + tributos(bruto, irrf).toFixed(2);
+            objResCSLL.innerHTML = "CSLL: R$" + tributos(bruto, csll).toFixed(2);
+            objResPIS.innerHTML = "PIS/PASEP: R$" + tributos(bruto, pis).toFixed(2);
+            objResCOFINS.innerHTML = "COFINS: R$" + tributos(bruto, cofins).toFixed(2);
 
         } else if (isNaN(objFloLiq) || (objFloLiq == 0)) {
 
@@ -43,6 +61,11 @@ window.onload = function () {
             objResValorBruto.innerHTML = "Valor Bruto: R$" + objFloBruto.toFixed(2);
             objResValorLiq.innerHTML = "Valor Líquido: R$" + liquido;
             objResValorImp.innerHTML = "Valor de Imposto: R$" + calcVlrImp(objFloBruto, liquido).toFixed(2);
+            objResIRRF.innerHTML = "IRRF: R$" + tributos(objFloBruto, irrf).toFixed(2);
+            objResCSLL.innerHTML = "CSLL: R$" + tributos(objFloBruto, csll).toFixed(2);
+            objResPIS.innerHTML = "PIS/PASEP: R$" + tributos(objFloBruto, pis).toFixed(2);
+            objResCOFINS.innerHTML = "COFINS: R$" + tributos(objFloBruto, cofins).toFixed(2);
+
 
         }
     }
